f687ac807c744fc78dd034e324b0fa4d7b4a64da
SERHII-YAKYMENKO <graff.gorshin@gmail.com>
2021-05-05 22:44:13 +0300

    started header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
 
+  $('.dropdown').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active')
+    $('.dropdown').toggleClass('dropdown--active')
+  });
+
 });
