59d70626228b6772d8ca236f42e2a1fb79ae9e83
SERHII-YAKYMENKO <graff.gorshin@gmail.com>
2021-05-07 12:52:59 +0300

    finish header

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
 $(function () {
 
-  $('.dropdown').on('click', function () {
+  $('.menu__dropdown').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active')
-    $('.dropdown').toggleClass('dropdown--active')
+    $('.menu__dropdown').toggleClass('menu__dropdown--active')
   });
   
 });
