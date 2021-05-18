9264760df00790c5a4792b7204bde1545711e46b
SERHII-YAKYMENKO <graff.gorshin@gmail.com>
2021-05-18 18:25:06 +0300

    added block categories

--- app/js/main.js
+++ app/js/main.js
@@ -11,8 +11,16 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
-    // autoplay: true,
-    // autoplaySpeed: 10000,
+    prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><style>.a{stroke-linejoin:round;stroke-width:2;stroke:#8D28AD;}</style><path d="M19 12H5" class="a"/><path d="M12 5L5 12 12 19" class="a"/></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><style>.a{stroke-linejoin:round;stroke-width:2;stroke:#8D28AD;}</style><path d="M5 12H19" class="a"/><path d="M12 5L19 12 12 19" class="a"/></svg></button>',
+    infinite: false,
+  });
+
+  $('.categories__List').slick({
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
   });
 
   $('.menu__dropdown').on('click', function () {
